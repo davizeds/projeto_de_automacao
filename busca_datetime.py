3263c291493b21f2a2a899d402a4fc2985b27bf3
davizeds <davinazario1234@hotmail.com>
2026-04-04 21:44:05 -0300

    continuçao da escolha de data

--- busca_datetime.py
+++ busca_datetime.py
@@ -1,18 +1,24 @@
-from colocar_caminho import caminho
+from colocar_caminho import pegar_caminho
 import os
 from datetime import datetime
+from dicionario import categorias
 
+def escolhendo_pasta():
+    while True:
+        pasta_escolhida = input( 'escolha sua pasta' ).lower()
+        if pasta_escolhida in categorias:
+            caminho_da_pasta_escolhida=os.path.join(pegar_caminho(), pasta_escolhida)
+            return caminho_da_pasta_escolhida
+        else: print('pasta nao encontrada ou digitada errado')
 
-
-pastas=os.path.join(caminho,'audios')
+pastas=escolhendo_pasta()
 print( os.listdir( pastas ) )
 arquivos_da_pasta=( os.listdir( pastas ) )
-
 
 def conferimento_de_data():
     while True:
         dia_desejado=int(input('dia desejado'))
-        encontrou=busca_datetime(arquivos_da_pasta,dia_desejado)
+        encontrou=busca_datetime(arquivos_da_pasta,dia_desejado,pastas)
         if encontrou == False:
             print ('nao achamos nada destes dias')
         resposta=input('quer continuar (s/n)').lower()
@@ -21,10 +27,11 @@ def conferimento_de_data():
 
 
 
-def busca_datetime(arquivos_da_pasta,dia_desejado,):
+
+def busca_datetime(arquivos_da_pasta,dia_desejado,pastas):
     encontrou = False
     for documentos in arquivos_da_pasta:
-        caminho_completo = os.path.join( caminho, 'audios', documentos)
+        caminho_completo = os.path.join(pastas,documentos)
         data_dos_arquivos=os.path.getmtime(caminho_completo)
         data_convertida=datetime.fromtimestamp(data_dos_arquivos)
         apenas_o_dia=data_convertida.day
