947de0a658e07f1b9f29e510492335e8082b4e47
davizeds <davinazario1234@hotmail.com>
2026-04-06 23:53:44 -0300

    feat: adiciona busca por mês, ano e data completa

--- busca_datetime.py
+++ busca_datetime.py
@@ -15,20 +15,17 @@ pastas=escolhendo_pasta()
 print( os.listdir( pastas ) )
 arquivos_da_pasta=( os.listdir( pastas ) )
 
-def conferimento_de_data():
+def conferimento_de_dia():
     while True:
         dia_desejado=int(input('dia desejado'))
-        encontrou=busca_datetime(arquivos_da_pasta,dia_desejado,pastas)
+        encontrou=buscar_day(arquivos_da_pasta,dia_desejado,pastas)
         if encontrou == False:
             print ('nao achamos nada destes dias')
         resposta=input('quer continuar (s/n)').lower()
         if resposta == 'n':
             break
 
-
-
-
-def busca_datetime(arquivos_da_pasta,dia_desejado,pastas):
+def buscar_day(arquivos_da_pasta,dia_desejado,pastas):
     encontrou = False
     for documentos in arquivos_da_pasta:
         caminho_completo = os.path.join(pastas,documentos)
@@ -37,5 +34,77 @@ def busca_datetime(arquivos_da_pasta,dia_desejado,pastas):
         apenas_o_dia=data_convertida.day
         if dia_desejado==apenas_o_dia:
             print(documentos, data_convertida)
+            encontrou = True
+    return encontrou
+
+def conferimento_de_mes():
+    while True:
+        mes_desejado=int(input('mes desejado'))
+        encontrou=buscar_mes(arquivos_da_pasta,mes_desejado,pastas)
+        if encontrou == False:
+            print ('nao achamos nada desse mes')
+        resposta=input('quer continuar (s/n)').lower()
+        if resposta == 'n':
+            break
+
+
+
+def buscar_mes(arquivos_da_pasta,mes_desejado,pastas):
+    encontrou = False
+    for documentos in arquivos_da_pasta:
+        caminho_completo = os.path.join(pastas,documentos)
+        data_dos_arquivos=os.path.getmtime(caminho_completo)
+        data_convertida=datetime.fromtimestamp(data_dos_arquivos)
+        apenas_o_mes=data_convertida.month
+        if mes_desejado==apenas_o_mes:
+            print(documentos, data_convertida)
+            encontrou = True
+    return encontrou
+
+def conferimento_de_ano():
+    while True:
+        ano_desejado=int(input('ano desejado'))
+        encontrou=buscar_ano(arquivos_da_pasta,ano_desejado,pastas)
+        if encontrou == False:
+            print ('nao achamos nada desse ano')
+        resposta=input('quer continuar (s/n)').lower()
+        if resposta == 'n':
+            break
+
+def buscar_ano(arquivos_da_pasta,ano_desejado,pastas):
+    encontrou = False
+    for documentos in arquivos_da_pasta:
+        caminho_completo = os.path.join(pastas,documentos)
+        data_dos_arquivos=os.path.getmtime(caminho_completo)
+        data_convertida=datetime.fromtimestamp(data_dos_arquivos)
+        apenas_o_ano=data_convertida.year
+        if ano_desejado==apenas_o_ano:
+            print(documentos, data_convertida)
+            encontrou = True
+    return encontrou
+
+def confermineto_data_completa():
+    while True:
+        dia_desejado = int( input( 'dia desejado' ) )
+        mes_desejado = int( input( 'mes desejado' ) )
+        ano_desejado = int( input( 'ano desejado' ) )
+        encontrou=buscar_data_completa(arquivos_da_pasta,dia_desejado,mes_desejado,ano_desejado,pastas)
+        if encontrou == False:
+            print ('nao achamos nada dessa data')
+        resposta=input('quer continuar (s/n)').lower()
+        if resposta == 'n':
+            break
+
+def buscar_data_completa(arquivos_da_pasta,dia_desejado,mes_desejado,ano_desejado,pastas):
+    encontrou=False
+    for documentos in arquivos_da_pasta:
+        caminho_completo = os.path.join( pastas, documentos )
+        data_dos_arquivos = os.path.getmtime( caminho_completo )
+        data_convertida = datetime.fromtimestamp( data_dos_arquivos )
+        apenas_o_dia= data_convertida.day
+        apenas_o_mes= data_convertida.month
+        apenas_o_ano= data_convertida.year
+        if dia_desejado==apenas_o_dia and mes_desejado==apenas_o_mes and ano_desejado==apenas_o_ano:
+            print( documentos, data_convertida )
             encontrou = True
     return encontrou
